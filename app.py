4ce5a05ca9532d391380895fb10d1be22be92768
Sajan Kumar Kar <sajan.kumar.kar@gmail.com>
2023-12-27 17:46:42 -0500

    Modified main with Host Port for deployment

--- app.py
+++ app.py
@@ -87,5 +87,7 @@ def predict():
         # return {'Prediction': output}
     return render_template('index.html')
 # %%
+# if __name__ == "__main__":
+#     app.run(debug=True)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
